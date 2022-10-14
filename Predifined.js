c62c691b130145eacd7f7c7aac8219c0cc556aea
nicholas kebut <nicholas.kebut@thejitu.com>
2022-10-14 10:03:47 +0300

    Added OPredefined

---
+++ Predifined.js
@@ -0,0 +1,25 @@
+// Predefined functions
+// eval()   function evaluates JavaScript code represented as a string and 
+// returns its completion value. The source is parsed as a script.
+
+// example
+
+console.log(eval('2+2'));
+// expected output is 4
+
+// Syntax
+
+// eval('string')
+
+// Parameters is a String 
+/*
+    A string representing a JavaScript expression, statement, or 
+    sequence of statements. The expression can include variables
+    and properties of existing objects.
+*/
+
+//Return Value
+/*
+    The completion value of evaluating the given code. If the completion
+    value is empty, undefined is returned.
+*/
